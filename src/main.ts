c654818fc58a6c5cf585fb480fd5cc71dc86c28f
Gloria Hornero <snootchie.boochies@gmail.com>
2024-04-06 11:39:19 +0200

    creates the character class

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 const CHARACHTERS_URL = 'https://rickandmortyapi.com/api/character';
 
+class Character {
+  constructor(
+    public id: number,
+    public name: string,
+    public status: "Dead" | "Alive",
+    public species: string,
+    public type: string,
+    public gender: "Male" | "Female",
+    public origin: string,
+    public location: string,
+    public image: string,
+  ){}
+}
+
 const getCharacters = async(pageId: number) => {
   const response = await fetch(`${CHARACHTERS_URL}?page=${pageId}`);
   return await response.json()
