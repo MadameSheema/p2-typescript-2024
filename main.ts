d4525ba916d741434b4da87af68fc0ac6ed0a740
Gloria Hornero <snootchie.boochies@gmail.com>
2024-04-07 12:32:41 +0200

    removes src folder

--- main.ts
+++ main.ts
@@ -35,6 +35,7 @@ const head = (title: string) => `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
+    
 </head>`
 
 const renderCharacters = (characters: Array<Character>) => {
