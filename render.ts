bfbef799598aa9f5015e85cf3f86f2e815d45552
Gloria Hornero <snootchie.boochies@gmail.com>
2024-04-14 19:32:40 +0200

    adjustments

--- render.ts
+++ render.ts
@@ -2,21 +2,21 @@ import { Character } from "./characters";
 import { mkdir, writeFile } from "fs/promises";
 
 const renderCharacters = (characters: Array<Character>): string => {
-  let html = "";
+  let html = '';
   for (const character of characters) {
-    html += `<a class="character" href="characters/${character.id}.html">
-      <img class=img-list src="${character.image}" />
-      <p>${character.name}</p>
-    </a>`;
+    html += `<a class="character" href="./characters/${character.id}.html">
+        <img class=img-list src="${character.image}" alt="${character.name} image"/>
+        <p>${character.name}</p>
+      </a>
+      `;         
   }
   return html;
 }
 
 const renderCharacterDetails = (character: Character): string => {
-  return `
-    <div class="details-container">
+  return `<div class="details-container">
         <div>
-          <img src="${character.image}" />
+          <img src="${character.image}" alt="${character.name} image"/>
         </div>  
         <div class="data">
           <h2>${character.name}</h2>
@@ -27,35 +27,36 @@ const renderCharacterDetails = (character: Character): string => {
           <p><span class="bolded">Origin:</span> ${character.origin}</p>
           <p><span class="bolded">Location:</span> ${character.location}</p>
         </div>
-    </div>`;
+      </div>`;
 }
 
-const pageTemplate = (html: string, divClass: string, path: string = '.'): string => {
-  return `
-  <html>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Rick&Morty Characters</title>
-      <link rel="stylesheet" href="${path}/styles.css">
-      <link rel="icon" href="${path}/images/portal.png" type="image/x-icon" sizes="128x128" />
-    </head>
-    <body>
-      <header>
-        <div class="logo-container">
-          <img src=${path}/images/logo.png />
-        </div>
-      </header>
-        <div class="${divClass}">
-          ${html}
-        </div>  
-    </body>
-  </html>`;
+const renderPage = (html: string, divClass: string, path: string = '.'): string => {
+  return `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rick&Morty Characters</title>
+    <link rel="stylesheet" href="${path}/styles.css">
+    <link rel="icon" href="${path}/images/portal.png" type="image/x-icon" sizes="128x128" />
+  </head>
+  <body>
+    <header>
+      <div class="logo-container">
+        <img src=${path}/images/logo.png alt="Rick&Morty logo"/>
+      </div>
+    </header>
+    <div class="${divClass}">
+      ${html}
+    </div>  
+  </body>
+</html>`;
 }
 
 export const createIndexPageWithCharacters = async (characters: Array<Character>): Promise<void> => {
   const charactersHtml = renderCharacters(characters);
-  const charactersPageHtml = pageTemplate(charactersHtml, 'characters-container');
+  const charactersPageHtml = renderPage(charactersHtml, 'characters-container');
+
   await writeFile('index.html', charactersPageHtml);
 }
 
@@ -67,7 +68,7 @@ export const createCharactersPages = async (characters: Array<Character>): Promi
 
   for (const character of characters) {
     characterDetailsHtml = renderCharacterDetails(character);
-    characterDetailsPageHtml = pageTemplate(characterDetailsHtml, 'character-container', '..');
+    characterDetailsPageHtml = renderPage(characterDetailsHtml, 'character-container', '..');
     await writeFile(`characters/${character.id}.html`, characterDetailsPageHtml);
   }
 }
